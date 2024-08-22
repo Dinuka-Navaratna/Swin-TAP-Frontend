f891a58a82e4f46f870bb57f307877dcc1b50b10
Dinuka Navaratna <Dinuka.navaratna@gmail.com>
2024-08-23 08:53:47 +1000

    HashRouter added for routing issue

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { Suspense, lazy } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.js";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import "./App.css";
-//const Header = lazy(() => import("./components/Header"));
-//const TopMenu = lazy(() => import("./components/TopMenu"));
+
 const HomeView = lazy(() => import("./views/Home"));
 const SignInView = lazy(() => import("./views/account/SignIn"));
 const SignUpView = lazy(() => import("./views/account/SignUp"));
@@ -32,50 +31,37 @@ const BlogDetailView = lazy(() => import("./views/blog/Detail"));
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <React.Fragment>
         <Header />
-        {/* <TopMenu /> */}
-        <Suspense
-          fallback={
-            <div className="text-white text-center mt-3">Loading...</div>
-          }
-        >
+        <Suspense fallback={<div className="text-white text-center mt-3">Loading...</div>}>
           <Routes>
-            <Route exact path="/" element={<HomeView/>} />
-            <Route exact path="/account/signin" element={<SignInView/>} />
-            <Route exact path="/account/signup" element={<SignUpView/>} />
-            <Route
-              exact
-              path="/account/forgotpassword"
-              element={<ForgotPasswordView/>}
-            />
-            <Route exact path="/account/profile" element={<MyProfileView/>} />
-            <Route exact path="/account/orders" element={<OrdersView/>} />
-            <Route exact path="/account/wishlist" element={<WishlistView/>} />
-            <Route
-              exact
-              path="/account/notification"
-              element={<NotificationView/>}
-            />
-            <Route exact path="/category" element={<ProductListView/>} />
-            <Route exact path="/product/detail" element={<ProductDetailView/>} />
-            <Route exact path="/star/zone" element={<StarZoneView/>} />
-            <Route exact path="/cart" element={<CartView/>} />
-            <Route exact path="/checkout" element={<CheckoutView />} />
-            <Route exact path="/invoice" element={<InvoiceView />} />
-            <Route exact path="/documentation" element={<DocumentationView/>} />
-            <Route exact path="/contact-us" element={<ContactUsView/>} />
-            <Route exact path="/support" element={<SupportView/>} />
-            <Route exact path="/blog" element={<BlogView/>} />
-            <Route exact path="/blog/detail" element={<BlogDetailView/>} />
-            <Route exact path="/500" element={<InternalServerErrorView/>} />
-            <Route path="*" element={<NotFoundView/>} />
+            <Route path="/" element={<HomeView />} />
+            <Route path="/account/signin" element={<SignInView />} />
+            <Route path="/account/signup" element={<SignUpView />} />
+            <Route path="/account/forgotpassword" element={<ForgotPasswordView />} />
+            <Route path="/account/profile" element={<MyProfileView />} />
+            <Route path="/account/orders" element={<OrdersView />} />
+            <Route path="/account/wishlist" element={<WishlistView />} />
+            <Route path="/account/notification" element={<NotificationView />} />
+            <Route path="/category" element={<ProductListView />} />
+            <Route path="/product/detail" element={<ProductDetailView />} />
+            <Route path="/star/zone" element={<StarZoneView />} />
+            <Route path="/cart" element={<CartView />} />
+            <Route path="/checkout" element={<CheckoutView />} />
+            <Route path="/invoice" element={<InvoiceView />} />
+            <Route path="/documentation" element={<DocumentationView />} />
+            <Route path="/contact-us" element={<ContactUsView />} />
+            <Route path="/support" element={<SupportView />} />
+            <Route path="/blog" element={<BlogView />} />
+            <Route path="/blog/detail" element={<BlogDetailView />} />
+            <Route path="/500" element={<InternalServerErrorView />} />
+            <Route path="*" element={<NotFoundView />} />
           </Routes>
         </Suspense>
         <Footer />
       </React.Fragment>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
