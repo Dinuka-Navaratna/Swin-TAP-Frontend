f30760c6b513ccd5cec68be84f2f3191ffb1f3ec
Dinuka Navaratna <Dinuka.navaratna@gmail.com>
2024-09-20 07:54:43 +1000

    My ads/inspections

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ const HomeView = lazy(() => import("./views/Home"));
 const SignInView = lazy(() => import("./views/account/SignIn"));
 const SignUpView = lazy(() => import("./views/account/SignUp"));
 const ForgotPasswordView = lazy(() => import("./views/account/ForgotPassword"));
-const OrdersView = lazy(() => import("./views/account/Inspections"));
+const AdsInspections = lazy(() => import("./views/account/AdsInspections"));
 const WishlistView = lazy(() => import("./views/account/Wishlist"));
 const NotificationView = lazy(() => import("./views/account/Notification"));
 const MyProfileView = lazy(() => import("./views/account/MyProfile"));
@@ -47,19 +47,12 @@ function App() {
             <Route exact path="/" element={<HomeView/>} />
             <Route exact path="/account/signin" element={<SignInView/>} />
             <Route exact path="/account/signup" element={<SignUpView/>} />
-            <Route
-              exact
-              path="/account/forgotpassword"
-              element={<ForgotPasswordView/>}
-            />
+            <Route exact path="/account/forgotpassword" element={<ForgotPasswordView/>} />
             <Route exact path="/account/profile" element={<MyProfileView/>} />
-            <Route exact path="/account/inspections" element={<OrdersView/>} />
+            <Route exact path="/account/inspections" element={<AdsInspections/>} />
+            <Route exact path="/account/ads" element={<AdsInspections/>} />
             <Route exact path="/account/wishlist" element={<WishlistView/>} />
-            <Route
-              exact
-              path="/account/notification"
-              element={<NotificationView/>}
-            />
+            <Route exact path="/account/notification" element={<NotificationView/>} />
             <Route exact path="/listing" element={<ProductListView/>} />
             <Route exact path="/listing/:id" element={<ProductDetailView/>} />
             <Route exact path="/star/zone" element={<StarZoneView/>} />
