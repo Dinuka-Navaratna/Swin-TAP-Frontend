1f4b4f036e0158f3786bdca7f502125ae222bce6
Dinuka Navaratna <Dinuka.navaratna@gmail.com>
2024-09-20 09:57:18 +1000

    Address autofill

--- src/views/product/Detail.jsx
+++ src/views/product/Detail.jsx
@@ -4,6 +4,7 @@ import { data } from "../../data";
 import { getSession } from "../../actions/session";
 import { toTitleCase } from '../../helpers/letterCaseChange'
 import axios from "axios";
+import './style.css';
 const CardFeaturedProduct = lazy(() => import("../../components/card/CardFeaturedProduct"));
 const CardServices = lazy(() => import("../../components/card/CardServices"));
 const Details = lazy(() => import("../../components/others/Details"));
@@ -18,6 +19,7 @@ const ProductDetailView = () => {
   const [isEditMode, setIsEditMode] = useState(false);
   const [isNew, setIsNew] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
+  const [suggestions, setSuggestions] = useState([]);
   const { id } = useParams();
   const detailsRef = useRef(null);
   const inspectionRef = useRef(null);
@@ -154,10 +156,10 @@ const ProductDetailView = () => {
         data = createDataIfDifferent(details, vehicleData);
         if (data) {
           console.log("Data to be sent:", data);
-          console.log("-------------------");
-          console.log("Saved Data:", JSON.stringify(vehicleData));
-          console.log("-------------------");
-          console.log("New Data:", JSON.stringify(details));
+          // console.log("-------------------");
+          // console.log("Saved Data:", JSON.stringify(vehicleData));
+          // console.log("-------------------");
+          // console.log("New Data:", JSON.stringify(details));
         } else {
           console.log("No differences found.");
         }
@@ -184,8 +186,12 @@ const ProductDetailView = () => {
             }
             window.location.href = "/listing/" + response.data.data._id;
           } else {
-            alert("An error occurred. Please try again.");
             console.log(JSON.stringify(response.data));
+            if (response.data.msg.includes('not allowed to be empty')) {
+              alert("All fields must be filled. Please try again.")
+            } else {
+              alert("An error occurred. Please try again.");
+            }
           }
         })
         .catch((error) => {
@@ -193,8 +199,6 @@ const ProductDetailView = () => {
           console.log(error);
         });
     }
-
-    setIsEditMode(false);
   };
 
   const handleCancelClick = () => {
@@ -224,6 +228,29 @@ const ProductDetailView = () => {
     // data['inspection_status'] = "not_requested";
 
     return Object.keys(data).length ? JSON.stringify(data) : null;
+  };
+
+  const fetchAddressSuggestions = async (query) => {
+    const apiKey = process.env.REACT_APP_GEOAPIFY_API_KEY;
+    const response = await fetch(`https://api.geoapify.com/v1/geocode/autocomplete?text=${encodeURIComponent(query)}&filter=countrycode:au&apiKey=${apiKey}`);
+    const data = await response.json();
+    return data.features;
+  };
+
+  const handleAddressChange = async (e) => {
+    const query = e.target.value;
+    setSuggestions([]);
+    if (query.length >= 3) {
+      const results = await fetchAddressSuggestions(query);
+      setSuggestions(results);
+    }
+  };
+
+  const handleSuggestionClick = (suggestion) => {
+    detailsAddress.current.value = suggestion.properties.address_line1 + ', ' + suggestion.properties.city || '';
+    detailsState.current.value = suggestion.properties.state_code || '';
+    detailsPostalCode.current.value = suggestion.properties.postcode || '';
+    setSuggestions([]);
   };
 
   return (
@@ -258,13 +285,12 @@ const ProductDetailView = () => {
                 />
               </div>
               <div className="col-md-7">
-                <br></br>
                 {sessionData && (isNew || sessionData.user_id === vehicleData.seller_id._id) && <>
                   {isEditMode && <span className="badge bg-dark me-2 float-right" onClick={handleCancelClick}>Cancel</span>}
                   {!isEditMode && <span className="badge bg-dark me-2 float-right" onClick={handleDeleteClick}>Delete</span>}
                   <span className="badge bg-primary me-2 float-right" onClick={isEditMode ? handleSaveClick : handleEditClick}>{isEditMode ? 'Save' : 'Edit'}</span>
                 </>}
-                <h1 className="fw-bold h5 d-inline me-2">{isEditMode ? <input type="text" className="form-control mw-180" ref={detailsTitle} defaultValue={vehicleData !== null ? vehicleData.title : ''} placeholder="Title" /> : toTitleCase(vehicleData.title)}</h1>
+                <h1 className="fw-bold h5 d-inline me-2">{isEditMode ? <input type="text" className="form-control mw-180" ref={detailsTitle} defaultValue={vehicleData !== null ? vehicleData.title : ''} placeholder="Title" /> : <>{vehicleData !== null ? toTitleCase(vehicleData.title) : ''}</>}</h1>
                 {!isEditMode && (
                   <>
                     <span className="badge bg-success me-2">New</span>
@@ -272,11 +298,11 @@ const ProductDetailView = () => {
                   </>
                 )}
                 <div className="">
-                  <span className="h5 me-2">{isEditMode ? <input type="text" className="form-control mw-180" ref={detailsPrice} defaultValue={vehicleData !== null ? vehicleData.price : ''} placeholder="Price" /> : '$ '+vehicleData.price}</span>
+                  <span className="h5 me-2">{isEditMode ? <input type="text" className="form-control mw-180" ref={detailsPrice} defaultValue={vehicleData !== null ? vehicleData.price : ''} placeholder="Price" /> : <>$ +{vehicleData !== null ? toTitleCase(vehicleData.price) : 'N/A'}</>}</span>
                   {!isEditMode && (vehicleData.inspection_status === "completed") && <> <i className="bi bi-patch-check-fill text-success me-1" /> AutoAssured </>}
                 </div>
                 <div>
-                  <p>{isEditMode ? <textarea className="form-control" ref={detailsDescription} defaultValue={vehicleData !== null ? vehicleData.description : ''} placeholder="Description" /> : vehicleData.description}</p>
+                  <p>{isEditMode ? <textarea className="form-control" ref={detailsDescription} defaultValue={vehicleData !== null ? vehicleData.description : ''} placeholder="Description" /> : <>{vehicleData !== null ? toTitleCase(vehicleData.description) : ''}</>}</p>
                   {!isEditMode ? <>
                     <p className="fw-bold mb-2 small">Vehicle Highlights</p>
                     <ul className="small">
@@ -289,14 +315,23 @@ const ProductDetailView = () => {
                       <ul className="small">
                         <li><b>Seller Name:</b> {vehicleData.seller_id && vehicleData.seller_id.name ? toTitleCase(vehicleData.seller_id.name) : "N/A"}</li>
                         <li><b>Email:</b> {vehicleData.seller_id && vehicleData.seller_id.email ? (vehicleData.seller_id.email).toLowerCase() : "N/A"}</li>
-                        <li><b>Address:</b> {toTitleCase(vehicleData.address)}{vehicleData !== null && ", "+vehicleData.state+" ("+vehicleData.postal_code+")"}</li>
+                        <li><b>Address:</b> {toTitleCase(vehicleData.address)}{vehicleData !== null && ", " + vehicleData.state + " (" + vehicleData.postal_code + ")"}</li>
                       </ul>
                     </details>
                   </> : <>
                     <div className="row col-md-12">
                       <div className="col-md-4">
                         <label htmlFor="postalCode">Address</label><br></br>
-                        <input className="form-control mw-180" type="text" ref={detailsAddress} defaultValue={vehicleData !== null ? vehicleData.address : ''} id="detailsAddress" placeholder="Address" />
+                        <input onChange={handleAddressChange} className="form-control mw-180" type="text" ref={detailsAddress} defaultValue={vehicleData !== null ? vehicleData.address : ''} id="detailsAddress" placeholder="Address" />
+                        {suggestions.length > 0 && (
+                          <ul className="suggestions">
+                            {suggestions.map((suggestion, index) => (
+                              <li key={index} onClick={() => handleSuggestionClick(suggestion)}>
+                                {suggestion.properties.address_line1}, {suggestion.properties.address_line2}
+                              </li>
+                            ))}
+                          </ul>
+                        )}
                       </div>
                       <div className="col-md-4">
                         <label htmlFor="inspectionDate">State</label><br></br>
