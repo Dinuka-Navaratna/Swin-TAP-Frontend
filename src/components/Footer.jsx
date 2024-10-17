a1c35475fefb1371bfd16f474261b09f631a9405
Lahiru Gayashan <gayashan0202@gmail.com>
2024-10-17 17:28:48 +1100

    Footer updated by remove dummy data and add user login for admin

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <footer>
-      <div className="container-fluid bg-primary">
+      {/* <div className="container-fluid bg-primary">
         <div className="row ">
           <div className="col-md-9 py-3 text-white">
             Get connected with us on social networks!
@@ -33,17 +33,15 @@ const Footer = () => {
             </Link>
           </div>
         </div>
-      </div>
+      </div> */}
       <div className="container-fluid bg-dark text-white">
         <div className="row ">
           <div className="col-md-3 py-3">
             <div className="h6">AutoAssure</div>
             <hr />
             <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book.
+            AutoAssure ensures trust and transparency in vehicle sales by connecting sellers, 
+            buyers, and certified mechanics for seamless, reliable transactions.
             </p>
           </div>
           <div className="col-md-3 py-3">
@@ -144,9 +142,9 @@ const Footer = () => {
             <address>
               <strong>AutoAssure</strong>
               <br />
-              Dummy dummy
+              John St, 
               <br />
-              Melbourne, VIC
+              Hawthorn VIC 3122
               <br />
               <hr />
               <i className="bi bi-telephone"></i>&nbsp;&nbsp;(123) 456-7890
